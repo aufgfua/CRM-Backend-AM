3636b975d1d2bbfd4965f158c3c9060dbdaa3b17
augusto.flach <augustofalcaoflach@gmail.com>
2022-07-16 03:42:00 -0300

    refact(customer): refactor customer model test code to fit new customer properties

---
+++ src/test/java/com/guxo/crm_backend/customer/CustomerTest.java
@@ -0,0 +1,24 @@
+package com.guxo.crm_backend.customer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerTest {
+    /**
+     * Method under test: {@link Customer#Customer(Long, String, String)}
+     */
+    @Test
+    void testConstructor() {
+        Customer actualCustomer = new Customer(123L, "Name", "Doe");
+
+        assertNull(actualCustomer.getDateOfBirth());
+        assertEquals("Doe", actualCustomer.getSurname());
+        assertEquals("Name", actualCustomer.getName());
+        assertNull(actualCustomer.getLastPurchase());
+        assertEquals(123L, actualCustomer.getId().longValue());
+        assertNull(actualCustomer.getEmail());
+    }
+}
+
