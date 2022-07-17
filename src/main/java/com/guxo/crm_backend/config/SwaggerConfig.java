1090a8af2618c99248b6a73a7e20196d26ea1ce5
augusto.flach <augustofalcaoflach@gmail.com>
2022-07-16 22:49:28 -0300

    refact(config): reconfigure application/swagger path and dependencies

--- src/main/java/com/guxo/crm_backend/config/SwaggerConfig.java
+++ src/main/java/com/guxo/crm_backend/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.guxo.crm_backend.swagger;
+package com.guxo.crm_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
