2f63af427a98ad7f3fd16f7d677836967ee133bc
augusto.flach <augustofalcaoflach@gmail.com>
2022-07-18 00:52:10 -0300

    refact(docs): change from swagger to OpenAPI + swagger-ui (apparently Swagger is 'deprecated')

---
+++ src/main/java/com/guxo/crmbackend/config/OpenAPISecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.guxo.crmbackend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info =@Info(
+                title = "CRM Backend API - A.M.",
+                version = "${api.version}",
+                contact = @Contact(
+                        name = "Augusto Flach", email = "augustofalcaoflach@gmail.com", url = "https://guxo.com.br/"
+                ),
+                description = "A simple CRM Backend API. Authentication through /login"
+        )
+)
+public class OpenAPISecurityConfiguration {
+
+    // That one is definitely not mine:
+    // https://www.baeldung.com/openapi-jwt-authentication
+
+
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+
+
+
+}
