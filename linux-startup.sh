781352ec68a77e6b88b7cfa422d5285fdf58ed6a
augusto.flach <augustofalcaoflach@gmail.com>
2022-07-19 04:10:47 -0300

    feat(init): create docker initialization script for linux (/linux-startup.sh)

---
+++ linux-startup.sh
@@ -0,0 +1,2 @@
+ docker build -t crm-api .
+ docker-compose up
--- src/main/java/com/guxo/crmbackend/auth/WebSecurityConfig.java
+++ src/main/java/com/guxo/crmbackend/auth/WebSecurityConfig.java
@@ -102,12 +102,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
-        super.configure(web);
-    }
-
 
     @Bean
     @Override
